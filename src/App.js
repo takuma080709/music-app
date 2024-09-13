52cbe98848f4c9c9d896782f82066424689be766
takuma080709 <takuma593@gmail.com>
2024-09-13 20:11:48 +0900

    create music-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { SongList } from "./components/SongList";
+import spotify from "./lib/spotify";
+import { useRef } from "react";
+import { Player } from "./components/Player";
+import { SearchInput } from "./components/SearchInput";
+import { Pagination } from "./components/Pagination";
+const limit = 20;
 
-function App() {
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [popularSongs, setpopularSongs] = useState([]);
+  const [isPlay, setIsPlay] = useState(false);
+  const [selectedSong, setSelectedSong] = useState();
+  const [keyword, setKeyword] = useState("");
+  const [searchedSongs, setSearchedSongs] = useState(null);
+  const [page, setPage] = useState(1);
+  const [hasNext, setHasNext] = useState(false);
+  const [hasPrev, setHasPrev] = useState(false);
+  const audioRef = useRef();
+  const isSearchedResults = searchedSongs != null;
+
+  useEffect(() => {
+    fetchPoplularSongs();
+  }, []);
+
+  const fetchPoplularSongs = async () => {
+    setIsLoading(true);
+    const result = await spotify.getPopularSongs();
+    const popularSongs = result.items.map((item) => {
+      return item.track;
+    });
+    setpopularSongs(popularSongs);
+    setIsLoading(false);
+  };
+
+  const handleSongSelected = async (song) => {
+    setSelectedSong(song);
+    if (song.preview_url !== null) {
+      audioRef.current.src = song.preview_url;
+      playSong();
+    } else {
+      pauseSong();
+    }
+  };
+
+  const playSong = () => {
+    audioRef.current.play();
+    setIsPlay(true);
+  };
+
+  const pauseSong = () => {
+    audioRef.current.pause();
+    setIsPlay(false);
+  };
+
+  const toggleSong = () => {
+    if (isPlay) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  };
+
+  const handleInputChange = async (e) => {
+    setKeyword(e.target.value);
+  }
+
+  const searchSongs = async (page) => {
+    setIsLoading(true);
+    const offset = parseInt(page) ? (parseInt(page) - 1) * limit : 0;
+    const result = await spotify.searchSongs(keyword, limit, offset);
+    setHasNext(result.next !== null);
+    setHasPrev(result.previous !== null);
+    setSearchedSongs(result.items);
+    setIsLoading(false);
+  }
+
+  const moveToNext = async () => {
+    const nextPage = page + 1;
+    await searchSongs(nextPage);
+    setPage(nextPage);
+  }
+
+  const moveToPrev = async () => {
+    const prevPage = page - 1;
+    await searchSongs(prevPage);
+    setPage(prevPage);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-1 p-8 mb-20">
+        <header className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold">Music App</h1>
+        </header>
+        <SearchInput onInputChange={handleInputChange} onSubmit={searchSongs}/>
+        <section>
+          <h2 className="text-2xl font-semibold mb-5">{isSearchedResults ? "Searched Results" : "Popular Songs"}</h2>
+          <SongList isLoading={isLoading} songs={isSearchedResults ? searchedSongs : popularSongs} onSongSelected={handleSongSelected}/>
+          {isSearchedResults && <Pagination onPrev={hasPrev ? moveToPrev : null} onNext={hasNext ? moveToNext: null} />}
+        </section>
+      </main>
+      {selectedSong != null && <Player song={selectedSong} isPlay={isPlay} onButtonClick={toggleSong} />}
+      <audio ref={audioRef} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/SongList.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+export function SongList(props) {
+    if (props.isLoading)
+      return (
+        <div className="inset-0 flex justify-center items-center">
+          <FontAwesomeIcon icon={faSpinner} spin size="3x" />
+        </div>
+      );
+
+    return (
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+        {props.songs.map((song) => {
+            return (
+              <div onClick={() => props.onSongSelected(song)} key={song.id} className="flex-none cursor-pointer ">
+                <img
+                  alt="thumbnail"
+                  src={ song.album.images[0].url }
+                  className="mb-2 rounded"
+                />
+                <h3 className="text-lg font-semibold">{song.name}</h3>
+                <p className="text-gray-400">By {song.artists[0].name}</p>
+              </div>
+            );
+        })}
+      </div>
+    );
+  }
